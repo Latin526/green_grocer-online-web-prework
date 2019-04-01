33c9952facf0dcc23e0cdfd98aef496b682d213c
Yasmin Torres <yasmintorres90@hotmail.com>
2019-04-01 12:15:31 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -8,6 +8,11 @@ end
 
 def apply_coupons(cart, coupons)
   # code here
+  {
+  "AVOCADO" => {:price => 3.0, :clearance => true, :count => 1},
+  "KALE"    => {:price => 3.0, :clearance => false, :count => 1},
+  "AVOCADO W/COUPON" => {:price => 5.0, :clearance => true, :count => 1},
+}
 end
 
 def apply_clearance(cart)
