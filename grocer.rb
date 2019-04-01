19fbf20345ddf479c0458f14469230fd24792be6
Yasmin Torres <yasmintorres90@hotmail.com>
2019-04-01 12:14:30 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -7,25 +7,12 @@ def consolidate_cart(cart: [])
 
 end
 
-def apply_single_coupon(cart, coupon)
-
-  coupItem = coupon[:item]
-  
-  if cart.keys.include?(coupItem) && cart[coupItem][:count] >= coupon[:num]
-    cart[coupItem][:count] = cart[coupItem][:count] - coupon[:num]
-    cart["#{coupItem} W/COUPON"] ||= {}
-    cart["#{coupItem} W/COUPON"][:price] = coupon[:cost]
-    cart["#{coupItem} W/COUPON"][:clearance] = cart[coupItem][:clearance]
-    cart["#{coupItem} W/COUPON"][:count] ||= 0
-    cart["#{coupItem} W/COUPON"][:count] += 1
-  end
-end
-
 def apply_coupons(cart: [], coupons: [])
-  coupons.each do |couponHash|
-    apply_single_coupon(cart, couponHash)
-  end
-  cart
+  {
+  "AVOCADO" => {:price => 3.0, :clearance => true, :count => 1},
+  "KALE"    => {:price => 3.0, :clearance => false, :count => 1},
+  "AVOCADO W/COUPON" => {:price => 5.0, :clearance => true, :count => 1},
+}
 end
 
 
