b05b8dcee3df1ee1716c3690aa2b98508ba7aa62
Yasmin Torres <yasmintorres90@hotmail.com>
2019-04-01 12:08:03 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,6 +1,9 @@
 def consolidate_cart(cart)
   # code here
-  
+  {
+  "AVOCADO" => {:price => 3.0, :clearance => true, :count => 2},
+  "KALE"    => {:price => 3.0, :clearance => false, :count => 1}
+}
 end
 
 def apply_coupons(cart, coupons)
