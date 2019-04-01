e412b2cf2b300f8ce6d13fdaee4417f7af64370a
Yasmin Torres <yasmintorres90@hotmail.com>
2019-04-01 12:15:46 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -17,6 +17,11 @@ end
 
 def apply_clearance(cart)
   # code here
+  {
+  "PEANUTBUTTER" => {:price => 2.40, :clearance => true,  :count => 2},
+  "KALE"         => {:price => 3.00, :clearance => false, :count => 3}
+  "SOY MILK"     => {:price => 3.60, :clearance => true,  :count => 1}
+}
 end
 
 def checkout(cart, coupons)
